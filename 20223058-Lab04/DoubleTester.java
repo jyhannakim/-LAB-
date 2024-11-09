1146e7e3957e27727051acf3e79e160b8d0e9d20
jyhannakim <130128753+jyhannakim@users.noreply.github.com>
2024-11-09 12:14:03 +0900

    Create DoubleTester.java

---
+++ 20223058-Lab04/DoubleTester.java
@@ -0,0 +1,11 @@
+
+public class DoubleTester {
+
+	public static void main(String[] args) {
+		double pro = 8.70;
+		int percentage = (int) Math.round (100 * pro);
+		System.out.println(percentage);
+
+	}
+
+}
